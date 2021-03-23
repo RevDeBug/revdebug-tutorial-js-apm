f39314ad3763662cce3b7fecd1e3a56055daaa93
Adam Kruszewski <adam@kruszewski.name>
2021-03-23 17:02:42 +0100

    Extend the query data test for completed and not completed items

--- cypress/integration/03-query-data.js
+++ cypress/integration/03-query-data.js
@@ -1,9 +1,22 @@
 describe('Query test data', () => {
       it('Query completed tasks', () => {
           cy.visit('http://localhost:3000/');
-          cy.get('#query').type("SELECT task_name, priority, is_completed FROM tasks WHERE is_completed=0");
+          cy.get('#query').type("SELECT * FROM tasks WHERE is_completed=1");
           cy.get('#send').click();
-          cy.get('#result').should('have.value',
-'[{"task_name":"buy grocieries","priority":5,"is_completed":0},{"task_name":"fill in taxes","priority":10,"is_completed":0},{"task_name":"feed the cat","priority":99,"is_completed":0}]');
+          cy.get('#result').should(($txtar) => {
+              expect($txtar).to.contain.value('read morning brew');
           })
+      })
+
+      it('Query not completed tasks', () => {
+          cy.visit('http://localhost:3000/');
+          cy.get('#query').type("SELECT * FROM tasks WHERE is_completed=0");
+          cy.get('#send').click();
+          cy.get('#result').should(($txtar) => {
+              expect($txtar).to.contain.value('feed the cat');
+              expect($txtar).to.contain.value('fill in taxes');
+              expect($txtar).to.contain.value('buy grocieries');
+          })
+      })
+
 })
